55520bdd59df495f3917f0a7ca5932ac8399717b
denlim <y.denny.yd@gmail.com>
2019-03-05 08:34:59 +0700

    buat menu notif dokter by denny

---
+++ app/src/main/java/com/example/lenovo/dokterapp/NotifDokter.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.dokterapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotifDokter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notif_dokter);
+    }
+}
